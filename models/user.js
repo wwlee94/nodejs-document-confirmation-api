0234f9adbf9ee38fdfa29bbd4b3b2095da3aec65
wwlee94 <wwlee94@naver.com>
2019-12-24 10:57:34 +0900

    에러 로그 및 에러 핸들링 처리

--- models/user.js
+++ models/user.js
@@ -4,21 +4,55 @@ require('dotenv').config();
 
 // 해쉬 함수
 function hash(password){
-    return  crypto.createHmac('sha256', process.env.HASH_SECRET_KEY).update(password).digest('hex');
+  return crypto.createHmac('sha256', process.env.HASH_SECRET_KEY).update(password).digest('hex');
 }
 
 // 스키마
 var User = mongoose.Schema({
-    email:{
-        type:String,
-        required:[true,'Email is required!'],
-        match:[/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,'Should be a vaild email address!'],
-        trim:true,
-        unique:true
-    },
-    password:{
-      type:String,
-      required:[true,'Password is required!'],
-      select:false
-    }
+  email:{
+    type:String,
+    required:[true,'Email is required!'],
+    match:[/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,'Should be a vaild email address!'],
+    trim:true,
+    unique:true
+  },
+  password:{
+    type:String,
+    required:[true,'Password is required!'],
+    select:false
+  }
   });
+
+User.path('password').validate(function(v) {
+  var user = this;
+
+  if(user.isNew){
+    if(!user.passwordConfirmation){
+      user.invalidate('passwordConfirmation', 'Password Confirmation is required!');
+    }
+    else {
+      if(user.password !== user.passwordConfirmation) {
+        user.invalidate('passwordConfirmation', 'Password Confirmation does not matched!');
+      }
+    }
+  }
+});
+
+// User.statics.findByEmail = function(email){
+//     return this.findOne({email}).exec();
+// }
+
+// 세이브 전 패스워드 해시 암호화
+User.pre('save', function (next){
+  var user = this;
+  user.password = hash(user.password);
+  return next();
+});
+
+User.methods.authenticatePassword = function(password) {
+  // 함수로 전달받은 password 의 해시값과, 데이터에 담겨있는 해시값과 비교를 합니다.
+  const hashed_password = hash(password);
+  return this.password === hashed_password;
+};
+
+module.exports = mongoose.model('User', User);
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 // env 설정
 require('dotenv').config();
@@ -9,12 +10,12 @@ console.log('Start Nodejs Web Server !');
 // MongoDB 설정
 mongoose.Promise = global.Promise;
 mongoose.connect( process.env.MONGO_DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true 
+  useNewUrlParser: true,
+  useUnifiedTopology: true 
 });
 var db = mongoose.connection;
 db.once('open', function () {
-   console.log('Successfully connected to MongoDB!');
+  console.log('Successfully connected to MongoDB!');
 });
 db.on('error', function (err) {
   console.log('MongoDB Error: ', err);
@@ -22,7 +23,7 @@ db.on('error', function (err) {
 
 var app = express();
 
-// 미들웨어
+// app 미들웨어
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(function (req, res, next) {
@@ -32,7 +33,20 @@ app.use(function (req, res, next) {
   next();
 });
 
-// Server 설정
+function logErrors(err, req, res, next) {
+  console.error(err.stack);
+  next(err);
+}
+
+function errorHandler(err, req, res, next) {
+  res.status(500);
+  res.render('error', { error: err });
+}
+
+app.use(logErrors);
+app.use(errorHandler);
+
+// server 설정
 var port = process.env.PORT || 3000;
 app.listen(port, function(){
   console.log('listening on port:' + port);
