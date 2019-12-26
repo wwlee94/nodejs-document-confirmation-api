fb58bfadea68a40f70e0d65f2a039d9b3a3df27b
wwlee94 <wwlee94@naver.com>
2019-12-26 21:37:52 +0900

    콜백 함수 -> Promise 사용하도록 수정

--- api/confirmations.js
+++ api/confirmations.js
@@ -16,11 +16,10 @@ Confirmations.post('/', Util.isLoggedin, validateParamsAndToken, validateDocumen
 
 module.exports = Confirmations;
 
-// * promise 사용 할건지 코드 일관성 있도록 구현할 것
-
 // 파라미터 & 토큰 검증
 function validateParamsAndToken(req, res, next){
 
+    console.error('파라미터 & 토큰 검증');
     // 파라미터 검증
     if (!req.body.id) return next(new Exception.InvalidParameterError('결재 서류 ID를 입력해주세요 !'));
     if (!req.body.email) return next(new Exception.InvalidParameterError('이메일을 입력해주세요 !'));
@@ -39,35 +38,44 @@ function validateParamsAndToken(req, res, next){
 // 결재 서류 검증
 function validateDocument(req, res, next){
 
-    var title = '';
-    // 결재 서류 ID 검증 및 confirm 가능한 문서인지
+    console.error('결재 서류 검증');
     Confirmation.find({ 'document': req.body.id }).populate('document', 'title confirmationOrder confirmedUsers').select('document')
-        .then((result) => { 
-            if (!result.length) return next(new Exception.InvalidParameterError('유효하지 않은 결재 서류 ID 입니다 !'));
-
-            doc = result[0].document;
-            title = doc.title;
-            confirmationOrder = doc.confirmationOrder.filter(x => !doc.confirmedUsers.includes(x));
-            if (confirmationOrder[0] !== req.body.email) return next(new Exception.ExceptionError(`해당 사용자는 결재 차례가 아닙니다 ! 다음 결재자는 '${confirmationOrder[0]}' 입니다.`));
-     
-            Document.updateOne({ _id: req.body.id }, { $push: { confirmedUsers: req.body.email }})
-                .catch((err) => { return next(new Exception.ExceptionError(err.message)); });
-
-            params = {
-                "document": req.body.id,
-                "userEmail": req.body.email,
-                "comment": req.body.comment || '',
-                "confirmation": req.body.confirmation
-            };
-        
-            var confirmation = new Confirmation(params);
-            confirmation.save()
-                .then((result) => res.send(Util.responseMsg(`'${title}' 제목의 문서를 결재했습니다.`)) )
-                .catch((err) => { return next(new Exception.ExceptionError(err.message)); });
-            return 'Hello';
-        })
-        .then((result) => console.log(result))
+        .then((result) => validateDocumentIdAndConfirm(result, req, next))
+        .then((result) => createConfirmation(result, req, res, next))
         .catch((err) => {
+            if (err instanceof Exception.ExceptionError) return next(err);
             return next(new Exception.ExceptionError(err.message));
         });
+};
+
+// 결재 서류 ID 검증 및 confirm 가능한 문서인지 검증
+function validateDocumentIdAndConfirm(result, req, next){
+
+    console.error('결재 서류 ID 검증 및 confirm 가능한 문서인지');
+    if (!result.length) throw new Exception.InvalidParameterError('유효하지 않은 결재 서류 ID 입니다 !');
+
+    doc = result[0].document;
+    title = doc.title;
+    confirmationOrder = doc.confirmationOrder.filter(x => !doc.confirmedUsers.includes(x));
+    if (confirmationOrder[0] !== req.body.email) throw new Exception.ExceptionError(`지금은 해당 사용자의 결재 차례가 아닙니다 ! 다음 결재자는 '${confirmationOrder[0]}' 입니다.`);
+
+    return Document.updateOne({ _id: req.body.id }, { $push: { confirmedUsers: req.body.email }});
+}
+
+// 결재 승인 생성
+function createConfirmation(result, req, res, next){
+
+    console.error('결재 승인 생성');
+    params = {
+        "document": req.body.id,
+        "userEmail": req.body.email,
+        "comment": req.body.comment || '',
+        "confirmation": req.body.confirmation
+    };
+
+    var confirmation = new Confirmation(params);
+    return confirmation.save()
+            .then((result) => {
+                res.send(Util.responseMsg(`['${title}'] 문서를 결재했습니다.`));
+            });
 };
